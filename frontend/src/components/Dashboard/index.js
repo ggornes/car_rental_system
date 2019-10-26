dfad140b4a914c36e9636dd6d9b68ccb3185ae1c
ggornes <gerardogornes@gmail.com>
2019-10-26 21:48:06 +0800

    show vehicles table on dashboard

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -5,89 +5,330 @@ import 'mdbreact/dist/css/mdb.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import {MDBBtn, MDBDataTable} from 'mdbreact';
 
-const data = {
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+const DEFAULT_QUERY = 'redux';
+
+/*
+const data2 = {
     columns: [
         {
-            label: 'Name',
-            field: 'name',
+            label: 'id',
+            field: 'id',
             sort: 'asc',
             width: 150
         },
         {
-            label: 'Position',
-            field: 'position',
+            label: 'Make',
+            field: 'make',
             sort: 'asc',
             width: 270
         },
         {
-            label: 'Office',
-            field: 'office',
+            label: 'Model',
+            field: 'model',
             sort: 'asc',
             width: 200
         },
         {
-            label: 'Age',
-            field: 'age',
+            label: 'Release Year',
+            field: 'release_year',
             sort: 'asc',
             width: 100
         },
         {
-            label: 'Start date',
-            field: 'date',
+            label: 'Rego',
+            field: 'registration',
             sort: 'asc',
             width: 150
         },
         {
-            label: 'Salary',
-            field: 'salary',
+            label: 'Fuel Type',
+            field: 'fuel',
+            sort: 'asc',
+            width: 100
+        },
+        {
+            label: 'Tank Size',
+            field: 'tank_size',
+            sort: 'asc',
+            width: 100
+        },
+        {
+            label: 'Initials',
+            field: 'initials',
+            sort: 'asc',
+            width: 100
+        },
+        {
+            label: 'Created',
+            field: 'created',
+            sort: 'asc',
+            width: 100
+        },
+        {
+            label: 'Updated',
+            field: 'updated',
             sort: 'asc',
             width: 100
         }
     ],
     rows: [
         {
-            name: 'Tiger Nixon',
-            position: 'System Architect',
-            office: 'Edinburgh',
-            age: '61',
-            date: '2011/04/25',
-            salary: '$320'
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 1,
+            "initials": "DC",
+            "make": "Ariel",
+            "model": "Atom 4",
+            "registration": "1ARI444",
+            "release_year": 2019,
+            "tank_size": "40.0",
+            "updated": null
         },
         {
-            name: 'Garrett Winters',
-            position: 'Accountant',
-            office: 'Tokyo',
-            age: '63',
-            date: '2011/07/25',
-            salary: '$170'
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 2,
+            "initials": "GG",
+            "make": "BMW",
+            "model": "X5",
+            "registration": "1BGZ784",
+            "release_year": 2006,
+            "tank_size": "93.0",
+            "updated": null
         },
         {
-            name: 'Ashton Cox',
-            position: 'Junior Technical Author',
-            office: 'San Francisco',
-            age: '66',
-            date: '2009/01/12',
-            salary: '$86'
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 3,
+            "initials": "HS",
+            "make": "Holden",
+            "model": "Commodore Sportwagon RS-V",
+            "registration": "1DWU656",
+            "release_year": 2019,
+            "tank_size": "50.0",
+            "updated": null
         },
         {
-            name: 'Cedric Kelly',
-            position: 'Senior Javascript Developer',
-            office: 'Edinburgh',
-            age: '22',
-            date: '2012/03/29',
-            salary: '$433'
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 4,
+            "initials": "KF",
+            "make": "Holden",
+            "model": "Barina",
+            "registration": "1KJH123",
+            "release_year": 2018,
+            "tank_size": "40.0",
+            "updated": null
         },
-
         {
-            name: 'Donna Snider',
-            position: 'Customer Support',
-            office: 'New York',
-            age: '27',
-            date: '2011/01/25',
-            salary: '$112'
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Diesel",
+            "id": 5,
+            "initials": "DL",
+            "make": "Ford",
+            "model": "Ranger XL",
+            "registration": "1GVL526",
+            "release_year": 2015,
+            "tank_size": "80.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Electric",
+            "id": 6,
+            "initials": "FM",
+            "make": "Tesla",
+            "model": "Roadster",
+            "registration": "8HDZ576",
+            "release_year": 2008,
+            "tank_size": "0.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Diesel",
+            "id": 7,
+            "initials": "MC",
+            "make": "Holden",
+            "model": "Commodore LT ",
+            "registration": "1GXI000",
+            "release_year": 2018,
+            "tank_size": "61.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 8,
+            "initials": "XX",
+            "make": "Mitsubishi",
+            "model": "Lancer",
+            "registration": "1QWE456",
+            "release_year": 2015,
+            "tank_size": "45.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 9,
+            "initials": "LW",
+            "make": "BMW",
+            "model": "Z4",
+            "registration": "HMM B33R",
+            "release_year": 2019,
+            "tank_size": "52.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Gas",
+            "id": 10,
+            "initials": "CP",
+            "make": "Hyundai",
+            "model": "Sonata Premium",
+            "registration": "ICTPRG403",
+            "release_year": 2018,
+            "tank_size": "70.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 11,
+            "initials": "MF",
+            "make": "Chevrlet",
+            "model": "Cadillac",
+            "registration": "C4D1LL4C",
+            "release_year": 1959,
+            "tank_size": "79.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 12,
+            "initials": "MG",
+            "make": "Ford",
+            "model": "Territory ",
+            "registration": "T3RR1TORY",
+            "release_year": 2014,
+            "tank_size": "55.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Diesel",
+            "id": 13,
+            "initials": "MC",
+            "make": "Mitsubishi",
+            "model": "Pajero GLS",
+            "registration": "2MCR657",
+            "release_year": 2019,
+            "tank_size": "90.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 14,
+            "initials": "DS",
+            "make": "Mitsubishi",
+            "model": "Eclipse Cross ES (2WD)",
+            "registration": "1GGG292",
+            "release_year": 2018,
+            "tank_size": "63.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Diesel",
+            "id": 15,
+            "initials": "CP",
+            "make": "Hyundai",
+            "model": "Santa Fe Highlander CRDi Satin Dark",
+            "registration": "ICTPRG418",
+            "release_year": 2019,
+            "tank_size": "71.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 16,
+            "initials": "DS",
+            "make": "Bugatti",
+            "model": "Veyron 16.4 Super Sport",
+            "registration": "1FST319",
+            "release_year": 2011,
+            "tank_size": "100.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 17,
+            "initials": "HV",
+            "make": "Toyota",
+            "model": "Yaris",
+            "registration": "YGT427",
+            "release_year": 2019,
+            "tank_size": "42.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Gas",
+            "id": 18,
+            "initials": "CP",
+            "make": "Hyundai",
+            "model": "Elantra SR Turbo",
+            "registration": "ICTPRG404",
+            "release_year": 2017,
+            "tank_size": "50.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Petrol",
+            "id": 19,
+            "initials": "HV",
+            "make": "Toyota",
+            "model": "Corolla Hatch",
+            "registration": "CHT185",
+            "release_year": 2019,
+            "tank_size": "50.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Diesel",
+            "id": 20,
+            "initials": "DL",
+            "make": "Land Rover",
+            "model": "Defender",
+            "registration": "BCZ581",
+            "release_year": 2015,
+            "tank_size": "60.0",
+            "updated": null
+        },
+        {
+            "created": "Fri, 25 Oct 2019 21:40:34 GMT",
+            "fuel": "Diesel",
+            "id": 21,
+            "initials": "MP",
+            "make": "Ford",
+            "model": "escape titanium",
+            "registration": "FR2917",
+            "release_year": 2019,
+            "tank_size": "64.0",
+            "updated": null
         }
     ]
 };
+
+ */
 
 
 class Dashboard extends Component {
@@ -101,41 +342,103 @@ class Dashboard extends Component {
         this.state = {
             board:{},
             key:'',
-            data: [
-                {
-                    "id": 0,
-                    "name": "Mayer Leonard",
-                    "city": "Kapowsin",
-                    "state": "Hawaii",
-                    "country": "United Kingdom",
-                    "company": "Ovolo",
-                    "favoriteNumber": 7
-                },
-                {
-                    "id": 1,
-                    "name": "Lucios Lucer",
-                    "city": "Kapowsin",
-                    "state": "Hawaii",
-                    "country": "United Kingdom",
-                    "company": "Ovolo",
-                    "favoriteNumber": 7
-                }
-            ]
-        }
-
-
+            rows: [],
+            hits: [],
+        };
 
     }
 
+    componentDidMount() {
+        fetch('http://127.0.0.1:5000/vehicles/show')
+            .then(response => response.json())
+            .then(data => this.setState({ rows: data })
+            );
+    }
+
+
     render(){
+        const { rows } = this.state;
+        console.log(rows);
+
+
+        const data2 = {
+            columns: [
+                {
+                    label: 'Created',
+                    field: 'created',
+                    sort: 'asc',
+                    width: 150
+                },
+                {
+                    label: 'Fuel Type',
+                    field: 'fuel',
+                    sort: 'asc',
+                    width: 270
+                },
+                {
+                    label: 'id',
+                    field: 'id',
+                    sort: 'asc',
+                    width: 200
+                },
+                {
+                    label: 'Initials',
+                    field: 'initials',
+                    sort: 'asc',
+                    width: 100
+                },
+                {
+                    label: 'Make',
+                    field: 'make',
+                    sort: 'asc',
+                    width: 150
+                },
+                {
+                    label: 'Model',
+                    field: 'model',
+                    sort: 'asc',
+                    width: 100
+                },
+                {
+                    label: 'Rego',
+                    field: 'registration',
+                    sort: 'asc',
+                    width: 100
+                },
+                {
+                    label: 'Release Year',
+                    field: 'release_year',
+                    sort: 'asc',
+                    width: 100
+                },
+                {
+                    label: 'Tank Size',
+                    field: 'tank_size',
+                    sort: 'asc',
+                    width: 100
+                },
+                {
+                    label: 'Updated',
+                    field: 'updated',
+                    sort: 'asc',
+                    width: 100
+                }
+            ],
+            rows
+        };
+
+
+
         return(
             <div>
-                dash
+
+                <p></p>
                 <MDBDataTable
                     striped
                     bordered
                     hover
-                    data={data}
+                    data={data2}
+
                 />
             </div>
         );
