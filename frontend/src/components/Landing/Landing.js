40659fd309d3d4562e519c34272a7d9dc0a2421b
ggornes <gerardogornes@gmail.com>
2019-10-29 13:56:32 +0800

    edited loading animation

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
-import { getVehicles, addToList2, deleteItem, updateItem } from '../../VehicleFunctions'
+import {Circle} from "styled-spinkit";
+
+
+
+
+
+
+
+
 
 const Landing = () => (
     <div>
         <h1>Landing Page</h1>
+        <Circle color="blue"
+                size="80"
+        />
     </div>
 );
 export default Landing;
