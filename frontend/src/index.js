6a590877863d7ede8bf089c3170f3a0ec3957d25
ggornes <gerardogornes@gmail.com>
2019-11-01 13:08:24 +0800

    imported dependencies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'mdbreact/dist/css/mdb.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
