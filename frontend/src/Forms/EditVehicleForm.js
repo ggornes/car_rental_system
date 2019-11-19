d58e44a537b015c21fc79837664d0bc12ca15153
ggornes <gerardogornes@gmail.com>
2019-11-19 15:41:20 +0800

    implemented validations

--- frontend/src/Forms/EditVehicleForm.js
+++ frontend/src/Forms/EditVehicleForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {updateItem} from "../VehicleFunctions";
-import {MDBBtn} from "mdbreact";
+import {MDBBtn, MDBCol, MDBRow} from "mdbreact";
 import {Redirect} from "react-router-dom";
 import {VehicleFormErrors} from "./VehicleFormErrors"
 
@@ -23,9 +23,12 @@ class EditVehicleForm extends Component {
                 initials: this.props.item.initials,
             },
             toBrowse: false,
-            formErrors: {make: '', model: ''},
+            formErrors: {make: '', model: '', release_year: '', registration: '', tank_size: ''},
             makeValid: true,
             modelValid: true,
+            yearValid: true,
+            registrationValid: true,
+            tank_sizeValid: true,
             formValid: true
         };
 
@@ -51,27 +54,45 @@ class EditVehicleForm extends Component {
         let fieldValidationErrors = this.state.formErrors;
         let makeValid = this.state.makeValid;
         let modelValid = this.state.modelValid;
+        let yearValid = this.state.yearValid;
+        let registrationValid = this.state.registrationValid;
+        let tank_sizeValid = this.state.tank_sizeValid;
 
         switch(fieldName) {
             case 'make':
-                makeValid = value.length >= 6;
-                fieldValidationErrors.make = makeValid ? '' : ' is too short';
+                makeValid = value.length >= 3;
+                fieldValidationErrors.make = makeValid ? '' : 'value is too short';
                 break;
             case 'model':
-                modelValid = value.length >= 6;
+                modelValid = value.length >= 1;
                 fieldValidationErrors.model = modelValid ? '': ' is too short';
+                break;
+            case 'release_year':
+                yearValid = value.length === 4;
+                fieldValidationErrors.year = yearValid ? '': 'invalid year';
+                break;
+            case 'registration':
+                registrationValid = value.length === 7;
+                fieldValidationErrors.registration = registrationValid ? '': ' must be 7 characters';
+                break;
+            case 'tank_size':
+                tank_sizeValid = value > 0;
+                fieldValidationErrors.tank_size = tank_sizeValid ? '': ' must be greater than 0';
                 break;
             default:
                 break;
         }
         this.setState({formErrors: fieldValidationErrors,
             makeValid: makeValid,
-            modelValid: modelValid
+            modelValid: modelValid,
+            yearValid: yearValid,
+            registrationValid: registrationValid,
+            tank_sizeValid: tank_sizeValid
         }, this.validateForm);
     }
 
     validateForm() {
-        this.setState({formValid: this.state.makeValid && this.state.modelValid});
+        this.setState({formValid: this.state.makeValid && this.state.modelValid && this.state.yearValid && this.state.registrationValid && this.state.tank_sizeValid});
     }
 
     onSubmit = (e) => {
@@ -113,44 +134,57 @@ class EditVehicleForm extends Component {
 
                 <form onSubmit={this.onSubmit}>
 
-                    <div className="form-row">
-                        <div className="col-4">
-                            <input type="text" className="form-control" name="make" value={this.state.vehicle.make} onChange={this.onChange} placeholder="Make"/>
-                        </div>
-                        <div className="col-6">
-                            <input type="text" className="form-control" name="model" value={this.state.vehicle.model} onChange={this.onChange} placeholder="Model"/>
-                        </div>
-                        <div className="col-2">
-                            <input type="text" className="form-control" name="release_year" value={this.state.vehicle.release_year} onChange={this.onChange} placeholder="Year"/>
-                        </div>
-                    </div>
+                    <MDBRow>
+                        <MDBCol md="5" className="mb-3">
+                            <label className="grey-text"> Make </label>
+                            <input type="text" className="form-control" name="make" value={this.state.vehicle.make} onChange={this.onChange} placeholder="Make" required/>
+                        </MDBCol>
+                        <MDBCol md="5" className="mb-3">
+                            <label className="grey-text"> Model </label>
+                            <input type="text" className="form-control" name="model" value={this.state.vehicle.model} onChange={this.onChange} placeholder="Model" required/>
+                        </MDBCol>
+                        <MDBCol md="2" className="mb-3">
+                            <label className="grey-text"> Year </label>
+                            <input type="number" pattern="[0-9]*" className="form-control" name="release_year" value={this.state.vehicle.release_year} onChange={this.onChange} placeholder="Year" required/>
+                        </MDBCol>
+                    </MDBRow>
 
-                    <div><br/></div>
-
-                    <div className="form-row">
-                        <div className="col-4">
+                    <MDBRow>
+                        <MDBCol md="5" className="mb-3">
+                            <label className="grey-text"> Registration Number </label>
                             <input type="text" className="form-control" name="registration" value={this.state.vehicle.registration} onChange={this.onChange} placeholder="Registration Number"/>
-                        </div>
-                    </div>
+                        </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow>
+                        <MDBCol md="4" className="mb-3">
+                            <label className="grey-text"> Fuel Type </label>
+                            <input type="text" className="form-control" name="fuel" value={this.state.vehicle.fuel} onChange={this.onChange} placeholder="Fuel Type"/>
+                        </MDBCol>
+                        <MDBCol md="4" className="mb-3">
+                            <label className="grey-text"> Tank Size </label>
+                            <input type="number" pattern="[0-9]*" className="form-control" name="tank_size" value={this.state.vehicle.tank_size} onChange={this.onChange} placeholder="Tank Size"/>
+                        </MDBCol>
+                        <MDBCol md="4" className="mb-3">
+                            <label className="grey-text"> Initials </label>
+                            <input type="text" className="form-control" name="initials" value={this.state.vehicle.initials} onChange={this.onChange} placeholder="Initials"/>
+                        </MDBCol>
+                    </MDBRow>
+
+
+
                     <div><br/></div>
 
-                    <div className="form-row">
-                        <div className="col-4">
-                            <input type="text" className="form-control" name="fuel" value={this.state.vehicle.fuel} onChange={this.onChange} placeholder="Fuel Type"/>
-                        </div>
-                        <div className="col-4">
-                            <input type="text" className="form-control" name="tank_size" value={this.state.vehicle.tank_size} onChange={this.onChange} placeholder="Tank Size"/>
-                        </div>
-                        <div className="col-4">
-                            <input type="text" className="form-control" name="initials" value={this.state.vehicle.initials} onChange={this.onChange} placeholder="Initials"/>
-                        </div>
-                    </div>
+
+                    <div><br/></div>
+
+
 
 
                     <div className="form-row">
-                        <MDBBtn type="submit" gradient="aqua" disabled={!this.state.formValid}>Save</MDBBtn>
-                        <MDBBtn onClick={this.onClear} gradient="peach">Clear</MDBBtn>
-                        <MDBBtn onClick={this.onCancel} gradient="rare-wind">Cancel</MDBBtn>
+                        <MDBBtn type="submit" color="dark-green" disabled={!this.state.formValid}>Save</MDBBtn>
+                        <MDBBtn onClick={this.onClear} color="yellow">Clear</MDBBtn>
+                        <MDBBtn onClick={this.onCancel} color="red">Cancel</MDBBtn>
                     </div>
 
                 </form>
