d2e530f084a1c98ca2c8c29578308fe6c01a9400
ggornes <gerardogornes@gmail.com>
2019-11-20 11:24:09 +0800

    implementing backend test

---
+++ backend/test.py
@@ -0,0 +1,14 @@
+from mysql4 import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+	
+	#Ensure that flask was set up correctly
+	def test_index(self):
+		tester = app.test_client(self)
+		response = tester.get('/', content_type='html/text')
+		self.assertEqual(response.status_code, 200)
+		
+	
+	if __name__ == '__main__':
+		unittest.main()
