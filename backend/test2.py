d4589f684a5bf26119708fc28bfcc28b901268ca
ggornes <gerardogornes@gmail.com>
2019-11-21 12:45:03 +0800

    implementing unit testing for backend

--- backend/test2.py
+++ backend/test2.py
@@ -1,6 +1,7 @@
 import unittest
 from flask_testing import TestCase
-from mysql4 import app, db, Vehicles
+from mysql4 import app, db, Vehicles, Rentals, Services
+from flask import jsonify
 
 
 
@@ -15,6 +16,11 @@ class BaseTestCase(TestCase):
         db.create_all()
         db.session.add(Vehicles("Honda", "Civic", "2010", "1BND321", "Petrol", "40", "TEST"))
         db.session.add(Vehicles("BMW", "X3", "2010", "1BND321", "Petrol", "40", "TEST"))
+        db.session.commit()
+        #vehicle = Vehicles.query.filter(Vehicles.id == 1).all()
+        #print(vehicle)
+        db.session.add(Rentals("1", "0", "100", "2010/12/01", "2010/12/02", "K"))
+        db.session.add(Services("1", "100", "2010/12/01"))
 		#db.session.add(User("admin", "ad@min.com", "admin"))
         db.session.commit()
 
@@ -27,7 +33,18 @@ class BaseTestCase(TestCase):
 #class FlaskTestCase(unittest.TestCase):
 class FlaskTestCase(BaseTestCase):	
 
-	# Ensure that Vehicles table was set up correctly
+
+# ################################################
+# ######
+# ######         TESTING API END POINTS
+# ######
+# ################################################
+
+# ################################################
+# ######             GET Methods
+# ###############################################
+
+	# Ensure that GET (all vehicles) works correctly
 	
 	def test_browse(self):
 		#tester = app.test_client(self)
@@ -38,18 +55,44 @@ class FlaskTestCase(BaseTestCase):
 		self.assertIn(b'BMW', response.data)
 		#print(response.data)
 		
+		
+		
+	# Ensure that GET (vehicle by id) works correctly
+	
 	def test_browse_vehicleById(self):
 		response = self.client.get('/vehicles/show/2', content_type='html/text')
 		#self.assertEqual(response.status_code, 200)
 		self.assertIn(b'BMW', response.data)
-		#print(response.data)
+		print(response.get_json()[0]["id"])
+		print(response.get_json()[0]["make"])
+		self.assertTrue(response.get_json()[0]["id"] == 2)
+		
+		
+	# Ensure that GET (all vehicles) works correctly
+	# Expect 'K' on the response as the rental_type is K (km)
+	
+	def test_all_rentals(self):
+		response = self.client.get('/vehicles/rentals/1', content_type='html/text')
+		self.assertIn(b'K', response.data)
+	
+		
 		
 	#def test_add_new_vehicle(self):
 	#	response = self.client.post('/vehicles/add', data=dict(make='Holden', model='Commodore', release_year='2004', registration='1QWE234', fuel='Petrol', tank_size='45', initials='TEST'), follow_redirects=True)
 	#	self.assertIn(b'Holden', response.data)
+
 	
+# ################################################
+# ######
+# ######         TESTING queries
+# ######
+# ################################################
+	
+	# Ensure that Vehicles query works correctly
 	def test_query(self):
 		vehicle = Vehicles.query.filter(Vehicles.id == 2).all()
+		print(vehicle[0].make)
+		self.assertFalse(vehicle[0].make == 'Honda')
 		self.assertTrue(vehicle[0].make == 'BMW', vehicle[0].model == 'X3')
     
         
