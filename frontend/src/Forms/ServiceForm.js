392a9d60454fca0da35a64295b67004a205783ae
ggornes <gerardogornes@gmail.com>
2019-11-21 15:49:58 +0800

    implementing Formik validations

--- frontend/src/Forms/ServiceForm.js
+++ frontend/src/Forms/ServiceForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {addFuelPurchase, addNewRental, addService} from "../VehicleFunctions";
 import {Redirect} from "react-router-dom";
 import {MDBCol, MDBRow} from "mdbreact";
+import {Formik} from "formik";
+import * as Yup from "yup";
 
 class FuelPurchaseForm extends Component {
 
@@ -47,20 +49,34 @@ class FuelPurchaseForm extends Component {
         if (this.state.toDetails === true) {
             return <Redirect to={`/details2/${this.props.vehicleId}`} />
         }
+
+        const validationSchema = Yup.object().shape({
+            odometer: Yup.number().min(1, "Must be greater than 0").required("Must enter a value")
+        });
+
         return(
 
-            <form id="newServiceForm" onSubmit={this.onSubmit}>
-                <MDBRow>
-                    <MDBCol md="6" className="mb-3">
-                        <label className="grey-text"> Odometer </label>
-                        <input type="number" pattern="[0-9]*" className="form-control" name="odometer" value={this.state.service.odometer} onChange={this.onChange} placeholder="Odometer"/>
-                    </MDBCol>
-                    <MDBCol md="6" className="mb-3">
-                        <label className="grey-text"> Date of service </label>
-                        <input type="date" className="form-control" name="serviced_at" value={this.state.service.serviced_at} onChange={this.onChange} placeholder="Date of Service"/>
-                    </MDBCol>
-                </MDBRow>
-            </form>
+            <Formik
+                initialValues={{odometer: "", serviced_at: ""}}
+                validationSchema={validationSchema}
+            >
+                {({values, errors, touched, handleChange, handleBlur, isValid}) => (
+                    <form id="newServiceForm" onSubmit={this.onSubmit}>
+                        {JSON.stringify(values)}
+                        <MDBRow>
+                            <MDBCol md="6" className="mb-3">
+                                <label className="grey-text"> Odometer </label>
+                                <input style={touched.odometer && errors.odometer ? {color: 'red'} : {color: 'green'}} type="number" pattern="[0-9]*" name="odometer" value={values.odometer} onChange={handleChange} onBlur={handleBlur} placeholder="Odometer"/>
+                            </MDBCol>
+                            <MDBCol md="6" className="mb-3">
+                                <label className="grey-text"> Date of service </label>
+                                <input type="date" className="form-group" name="serviced_at" value={values.serviced_at} onChange={handleChange} onBlur={handleBlur} placeholder="Date of Service"/>
+                            </MDBCol>
+                        </MDBRow>
+                    </form>
+                )}
+            </Formik>
+
 
         );
     }
