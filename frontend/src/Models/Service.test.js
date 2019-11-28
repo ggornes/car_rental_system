7b47dfb9f2b7fb72049d88795b9f692c6d5792b1
ggornes <gerardogornes@gmail.com>
2019-11-28 10:49:42 +0800

    implementing unit test for Service class

--- frontend/src/Models/Service.test.js
+++ frontend/src/Models/Service.test.js
@@ -3,9 +3,9 @@ import {Service} from "./Service";
 describe ('Service', () => {
     it('calculates the number of services done', () => {
         const services = [
-            new Service('fake_id', 500, '2018-01-01'),
-            new Service('fake_id', 1000, '2018-02-01'),
-            new Service('fake_id', 2000, '2018-03-01')
+            new Service('dummy_id', 500, '2018-01-01'),
+            new Service('dummy_id', 1000, '2018-02-01'),
+            new Service('dummy_id', 2000, '2018-03-01')
         ];
 
         const expected = 3;
@@ -17,13 +17,13 @@ describe ('Service', () => {
 
     it('calculates the last service odometer correctly', () => {
         const services = [
-            new Service('fake_id', 500, '2018-01-01'),
-            new Service('fake_id', 1000, '2018-02-01'),
-            new Service('fake_id', 5000, '2017-02-01'),
-            new Service('fake_id', 2000, '2018-03-01')
+            new Service('dummy_id', 500, '2018-01-01'),
+            new Service('dummy_id', 1000, '2018-02-01'),
+            new Service('dummy_id', 5000, '2017-02-01'),
+            new Service('dummy_id', 2000, '2018-03-01')
         ];
 
-        // sort services descending odometer 
+        // sort services descending odometer
         const services2 = services.sort(function (a, b) {
             return b.odometer - a.odometer;
         });
